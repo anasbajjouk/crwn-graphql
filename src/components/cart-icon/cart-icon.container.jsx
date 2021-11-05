7464446eae4d2fb86c46fe668daf797cef9f1778
Anas Bajjouk <bajjouk1@gmail.com>
2021-11-05 15:05:56 +0100

    added graphql

---
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Mutation, Query, graphql } from "react-apollo";
+import { flowRight } from "lodash";
+import { gql } from "apollo-boost";
+import CartIcon from "./cart-icon.component";
+
+const TOGGLE_CART_HIDDEN = gql`
+  mutation ToggleCartHidden {
+    toggleCartHidden @client
+  }
+`;
+
+const GET_ITEM_COUNT = gql`
+  {
+    itemCount @client
+  }
+`;
+
+const CartIconContainer = ({ data: { itemCount }, ToggleCartHidden }) => (
+  <CartIcon toggleCartHidden={ToggleCartHidden} itemCount={itemCount} />
+);
+
+export default flowRight(
+  graphql(GET_ITEM_COUNT),
+  graphql(TOGGLE_CART_HIDDEN, { name: "toggleCartHidden" })
+)(CartIconContainer);
